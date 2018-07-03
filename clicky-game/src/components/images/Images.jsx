36e1531aecfb0a6ed52b677922afcdd7510cfb09
Jeff Hinzmann <JAHinzmann@gmail.com>
2018-07-03 17:54:18 -0400

    built more of the file structure

---
+++ clicky-game/src/components/images/Images.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import'./images.css'
+
+const Image = props => (
+    <div role='img' onClick={()=>props.'nameofFunction' (props.id)}
+    className={'imageTiles'} style={{backgroundImage: `url('${props.image}')`}}
+
+)
